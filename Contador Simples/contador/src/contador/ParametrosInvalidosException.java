b4698176aa11215d44bc8d75f0f97f3c375d6f34
pedrobotelho15 <pedrobotelho15@alu.ufc.br>
2022-08-09 17:48:12 -0300

    Projeto Contador completo.

---
+++ Contador Simples/contador/src/contador/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package contador;
+
+public class ParametrosInvalidosException extends RuntimeException {
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+    
+}
